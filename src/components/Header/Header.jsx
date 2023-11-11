0598fa9ee165d9219ffda7a3d6763877ec9da273
AlekseiMakonovitskii <alexmakondev@gmail.com>
2023-11-11 17:38:42 +0300

    added some header details

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
 	return (
 		<section className={styles.header}>
 			<div className={styles.searchBox}>
-				<select name="" id="">
+				<select name="" id="" className={styles.select}>
 					<option value="">Name</option>
 					<option value="">Age</option>
 					<option value="">Gender</option>
@@ -13,8 +13,8 @@ const Header = () => {
 					<option value="">Zip</option>
 					<option value="">Status</option>
 				</select>
-				<input type="text" />
-				<button>Search</button>
+				<input type="text" className={styles.search}/>
+				<button className={styles.searchBtn}>Search</button>
 			</div>
 		</section>
 	)
