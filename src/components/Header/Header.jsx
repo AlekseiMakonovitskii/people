71ec2b9fa6d4b830819442c4a4c2b270c2736368
AlekseiMakonovitskii <alexmakondev@gmail.com>
2023-11-11 17:29:31 +0300

    Basic header elements

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,20 @@ import styles from './Header.module.scss'
 
 const Header = () => {
 	return (
-		<div>Header</div>
+		<section className={styles.header}>
+			<div className={styles.searchBox}>
+				<select name="" id="">
+					<option value="">Name</option>
+					<option value="">Age</option>
+					<option value="">Gender</option>
+					<option value="">Phone</option>
+					<option value="">Zip</option>
+					<option value="">Status</option>
+				</select>
+				<input type="text" />
+				<button>Search</button>
+			</div>
+		</section>
 	)
 }
 
