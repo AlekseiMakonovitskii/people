13a02844c8368b9ecfaf8333fb95ef2c323d2360
AlekseiMakonovitskii <alexmakondev@gmail.com>
2023-11-12 16:40:15 +0300

    added fetch data logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './components/Header/Header'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchDataAsync, filteredDataAction } from './store'
 
 const App = () => {
+  const dispatch = useDispatch();
+  const globalData = useSelector(state => state.fetchDataReducer.globalData);
+  // const filteredData = useSelector(state => state.filteredDataReducer.filteredData)
+
+  useEffect(() => {
+    dispatch(fetchDataAsync());
+  }, [])
+
+  useEffect(() => {
+    dispatch(filteredDataAction(globalData))
+  }, [globalData])
+
+
   return (
     <div className='app'>
       <Header />
